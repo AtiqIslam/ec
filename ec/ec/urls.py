646df6964e7e781343713505abf0c23fefdcd967
AtiqIslam <atiq.sqa29@gmail.com>
2026-07-05 21:11:01 +0600

    Fix path

--- ec/ec/urls.py
+++ ec/ec/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('h/', include('commerce.urls')),
+    path('', include('commerce.urls')),
 ]
